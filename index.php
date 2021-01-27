9519db5918d58ff13ac6dfcba3178d1499f49f88
Mateusz <raczkamateusz7@gmail.com>
2021-01-27 10:48:04 +0100

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+define('_ROOT_PATH', dirname(__FILE__));
+
+$actions = array('home'); //wpisywać stworzone podstrony
+$action = 'home'; //zaczyna od...
+
+if(array_key_exists('action', $_GET))
+{
+    if(in_array($_GET['action'], $actions))
+    {
+        $action = $_GET['action'];
+    }
+    else
+    {
+        $action = 'pageNotFound';
+    }
+}
+
+include(_ROOT_PATH.DIRECTORY_SEPARATOR.'actions'.DIRECTORY_SEPARATOR.$action.'.php');
+include(_ROOT_PATH.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$action.'.php');
+
