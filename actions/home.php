c5c8edde34a7803c6be551721f0c2dfa52091b98
Mateusz <raczkamateusz7@gmail.com>
2021-01-27 13:30:45 +0100

    testowe połączenie z bazą

--- actions/home.php
+++ actions/home.php
@@ -1,1 +1,12 @@
 <?php
+
+$dbh = ibase_connect('127.0.0.1:c:\bazy\PROJEKT.FDB', 'SYSDBA', 'masterkey');
+$query = "SELECT test FROM TEST";
+
+$sth = ibase_query($dbh, $query);
+while ($row = ibase_fetch_object($sth)) {
+    echo $row->TEST, "\n";
+}
+
+ibase_free_result($sth);
+ibase_close($dbh);
