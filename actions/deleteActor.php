fbd74cca10c6a7cb479933107f99e8901a0304c4
kkuzm <kkuzmicki@wp.pl>
2021-02-09 22:08:33 +0100

    KK

--- actions/deleteActor.php
+++ actions/deleteActor.php
@@ -1,2 +1,19 @@
 <?php
-
+if(isset($_GET['id'])) {
+    $_SESSION['ID'] = $_GET['id'];
+}
+$dbh = new PDO('firebird:dbname=127.0.0.1:C:\bazy\PROJEKT.FDB', 'SYSDBA', 'masterkey');
+if(isset($_GET['confirmed'])) {
+    $query = $dbh->query("DELETE FROM AKTORZY WHERE ID_AKTOR=".$_SESSION['ID']);
+    $query = $dbh->query("DELETE FROM OBSADA WHERE ID_AKTOR=".$_SESSION['ID']);
+    header('Location: index.php?action=categoriesList');
+} else {
+    $query = $dbh->query("SELECT IMIE, NAZWISKO FROM AKTORZY WHERE ID_AKTOR=".$_SESSION['ID']);
+    foreach($query as $row) {
+        $name = $row['IMIE'];
+        $surname = $row['NAZWISKO'];
+    }
+}
+if(empty($name) || empty($surname)) {
+    header('Location: index.php?action=categoriesList');
+}
