945a696e5576705cb5cada029f0fd866e0b32cf4
kkuzm <kkuzmicki@wp.pl>
2021-02-22 23:22:25 +0100

    kk

--- actions/castList.php
+++ actions/castList.php
@@ -1,1 +1,15 @@
 <?php
+include 'Actor.php';
+
+if(isset($_GET['id'])) {
+    $_SESSION['ID'] = $_GET['id'];
+}
+
+$dbh = new PDO('firebird:dbname=127.0.0.1:C:\bazy\PROJEKT.FDB', 'SYSDBA', 'masterkey');
+$query = $dbh->query("SELECT * FROM OBSADA o INNER JOIN AKTORZY a ON o.ID_AKTOR = a.ID_AKTOR WHERE o.ID_FILM = ".$_SESSION['ID']);
+
+$i = 0;
+foreach($query as $row) {
+    $actors[$i] = new Actor($row['ID_AKTOR'], $row['IMIE'], $row['NAZWISKO']);
+    $i++;
+}
--- views/castList.php
+++ views/castList.php
@@ -1,1 +1,28 @@
+<html lang="pl-PL">
+
+<head>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/fontello.css">
+    <title>Lista aktorów</title>
+</head>
+<body>
 <?php
+include "topMenu.php";
+?>
+<main>
+    <a href="index.php?action=addCast" class="smallButton">Dodaj aktora...</a>
+    <?php
+    echo '<table border=1>';
+    echo '<tr><th>Aktor</th><th>Akcje</th></tr>';
+    $count = $i;
+    for($i = 0; $i < $count; $i++) {
+        echo '<tr><td>';
+        echo $actors[$i]->getIMIE().' '.$actors[$i]->getNAZWISKO();
+        echo '</td><td>';
+        echo '<a href="index.php?action=deleteActor" class="smallButtonV2">Usuń</a>';
+        echo '</td></tr>';
+    }
+    echo '</table>';
+    ?>
+
+</main>
