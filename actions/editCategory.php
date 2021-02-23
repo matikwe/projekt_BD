fe2f564e6be9c39f0468959c1679d6dda15e2069
kkuzm <kkuzmicki@wp.pl>
2021-02-23 04:02:24 +0100

    kk

--- actions/editCategory.php
+++ actions/editCategory.php
@@ -5,11 +5,12 @@ if(isset($_GET['id'])) {
 }
 
 if(isset($_POST['name'])) {
+    $category = strtolower($_POST['name']);
     $dbh = new PDO('firebird:dbname=127.0.0.1:C:\bazy\PROJEKT.FDB', 'SYSDBA', 'masterkey');
-    $query = $dbh->query("SELECT COUNT(KATEGORIA) FROM KATEGORIE WHERE KATEGORIA='".$_POST['name']."'");
+    $query = $dbh->query("SELECT COUNT(KATEGORIA) FROM KATEGORIE WHERE KATEGORIA='".$category."'");
     $count = $query->fetchColumn();
     if($count <= 0) {
-        $query = $dbh->query("UPDATE KATEGORIE SET KATEGORIA='" . $_POST['name'] . "' WHERE ID_KATEGORIA=" . $_SESSION['ID']);
+        $query = $dbh->query("UPDATE KATEGORIE SET KATEGORIA='" . $category . "' WHERE ID_KATEGORIA=" . $_SESSION['ID']);
         header('Location: index.php?action=categoriesList');
     } else {
         $_SESSION['error'] = 'Kategoria o takich danych już istnieje';
